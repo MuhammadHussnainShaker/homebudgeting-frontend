0efc40a5d29989da1167fc6c0b4aaec7c65b3aa2
Muhammad Hussnain Shaker <mhussnainshaker251@gmail.com>
2026-01-19 15:40:55 +0500

    chore: update TODO comment for backend logout functionality

--- src/components/Header/LogoutBtn.jsx
+++ src/components/Header/LogoutBtn.jsx
@@ -3,5 +3,7 @@ import useUserStore from '../../store/useUserStore'
 export default function LogoutBtn() {
   const logout = useUserStore((state) => state.logout)
 
+  // TODO: create logout functionality on backend and use that to clear accessToken from cookies
+
   return <button onClick={logout}>Logout</button>
 }
